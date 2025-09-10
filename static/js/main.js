a30b30c4a68236bcae57fb35011681dd497ac5cb
fabetabilo <fab.tabilo@duocuc.cl>
2025-09-10 19:46:38 -0300

    Estructura basica navbar-menu colapsable

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+/* main.js JavaScript, es un punto de entrada, me contiene:
+la logica, configuracion inicial y estructura principal de la interfaz de usuario */
+
+// para el menu movil
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", e => {
+    navLinks.classList.toggle("open");
+    // alterna entre el icono del menu, una X o el hamburguesa respectivamente
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-3-line");
+
+});
+
+navLinks.addEventListener("click", e => {
+    if  (e.tagName === "A") {   // de esta manera me aseguro, que solo se cerrara al hacer click en una opcion <a>
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+    }
+});
+
